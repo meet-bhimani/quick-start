4e820c3dc638db948ba74355a0fd4c5436c578f2
meet-bhimani <meet.bhimani@bacancy.com>
2024-07-07 22:58:43 +0530

    add global loader for whole app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { PuffLoader } from "react-spinners";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Suspense
+        fallback={
+          <div className='w-svw h-svh grid place-items-center'>
+            <PuffLoader size={50} color='#388da8' className='animate-spin' />
+          </div>
+        }>
+        <App />
+      </Suspense>
     </QueryClientProvider>
   </React.StrictMode>
 );
